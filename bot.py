3f477576bc7f6eb864e7f013b5d7298499072b9c
Hajisoy Mikayil <mikayilhajisoy@gmail.com>
2024-08-21 15:34:29 +0400

    1

--- bot.py
+++ bot.py
@@ -27,5 +27,6 @@ def ban_user(message):
 def make_some(message):
     bot.send_message(message.chat.id, 'I accepted a new user!')
     bot.approve_chat_join_request(message.chat.id, message.from_user.id)
-    
+
 bot.infinity_polling(none_stop=True)
+
